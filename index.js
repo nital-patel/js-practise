5d22cc994c64fe11b642db5cbfbf047df2dfcc2b
Nital Patel <patelnital36@gmail.com>
2019-02-25 14:42:27 -0500

    Add object & dot notation

---
+++ index.js
@@ -0,0 +1,16 @@
+let title = "Hello world"
+console.log(title);
+
+let interestRate = 0.3
+interestRate = 1;
+console.log(interestRate);
+
+//object
+let person = {
+    name: 'Mike',
+    age: 30
+};
+
+//dot notation
+person.name = 'John';
+console.log(person.name);
