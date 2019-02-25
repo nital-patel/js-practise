bcfec23850d2d0c9ae62dbcf31220a83705a3f0b
Nital Patel <patelnital36@gmail.com>
2019-02-25 16:17:26 -0500

    Add function with loop

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ let person = {
 //dot notation
 person.name = 'John';
 console.log(person.name);
+
+//array
+let selectorColors = ['red', 'blue'];
+selectorColors[2] = 'green';
+console.log(selectorColors.length);
+
+//function
+//perfoming a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ', 'Smith');
+}
+greet('John', 'Smith');
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+
+function doSometing() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+}
+doSometing();
